f6d0751ac5f879d099cef1cadeb41149829112eb
Alex Vergara <pyzyryab@tutanota.com>
2022-06-18 18:09:18 +0200

    At least, compiles

--- main.cpp
+++ main.cpp
@@ -7,29 +7,20 @@ import <string>;
 using namespace zero;
 
 // Mock class for test the process of making a Zero::Test
-class MockedClass {
+class MockedClass : Test {
     public:
-        std::string return_hi() const {
-            return std::string {"Hi!"};
-        }
-
-        int return_num() const {
-            return 10;
+        void operator()() {
+            std::cout << "Functor called" << std::endl;
         }
 };
 
 int main() {
-
-    // Prints the return values of the declared methods of the MockedClass
-    MockedClass mc;
-    std::cout << mc.return_hi() << std::endl;
-    std::cout << mc.return_num() << std::endl;
-
-    // Generate a new TestBuilder
-    // testing::core::TestBuilder itce = 
-    //     testing::core::TestBuilder<MockedClass> {mc};
     using namespace zero::testing::core;
-    TestSuite testSuite = TestSuite {};
+
+    TestSuite testSuite;
+    testSuite.add_test(MockedClass());
+    testSuite.run_tests();
+    
     return 0;
 }
     
--- zero/ifc/testing/testing_core.cppm
+++ zero/ifc/testing/testing_core.cppm
@@ -2,7 +2,7 @@ export module testing.core;
 
 export import builders;
 
-import <vector>;
+export import <vector>;
 import <string>;
 import <functional>;
     
@@ -33,6 +33,24 @@ export namespace zero::testing::core {
                 return this->tests;
             }
     };
+
+    /* Class Test<T>;
+        Builder should create a Test<T>, where T should reprensent
+        the Tested class. 
+
+        Every Test should be added to the TestSuite, which could
+        be initialized in the main test executable or as
+        a glob const static entity.  
+
+        Also, it could be made it wrapping main, and wiring the int main()
+        signature by macro?
+
+        So add_test() should receive only a Test object (maybe by inheritance)
+        of abstract base class?
+            TestThis : Test {...}
+
+            ?=?¿!?!¿?!¿!
+    */
 
     // template<class T>
     class TestSuite {
