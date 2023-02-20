fbb9d260bb8dba5ba2aedeaa8f1b948ae54fd49f
Alex Vergara <pyzyrya@tutanota.com>
2023-02-20 11:39:19 +0100

    Cleaning up a little bit the entry point of the program in main with the examples

--- main.cpp
+++ main.cpp
@@ -5,28 +5,42 @@ import iterator;
 import container;
 import type_info;
 
-// using namespace zero;
+using namespace zero;
+
+// Forward decls
+void run_containers_examples();
+void run_output_iterator_examples();
 
 int main() {
-    // constexpr auto a = collections::Array<long, 5>{1L, 2L, 3L, 4L, 5L};
-    // const Container<collections::Array<long, 5>>& b = collections::Array<long, 5>{1L, 2L, 3L, 4L, 5L};
-    // Container<collections::Array<long, 5>>* c = new collections::Array<long, 5>{1L, 2L, 3L, 4L, 5L};
-
-    // std::cout << "Iterating over the values of a constexpr zero::collection!" << std::endl;
-    // std::cout << "decltype a: " << zero::types::type_name<decltype(a)>() << std::endl;
-    // for (long value : a)
-    //     std::cout << " - [constexpr] Value: " << value << std::endl;
-
-    // std::cout << "Iterating over the values of a zero::container!" << std::endl;
-    // std::cout << "decltype b: " << zero::types::type_name<decltype(b)>() << std::endl;
-    // for (long value : b)
-    //     std::cout << " - [const Container<T>&] Value: " << value << std::endl;
-
-    // std::cout << "Iterating over the values of a zero::container dynamically allocated!" << std::endl;
-    // std::cout << "decltype c: " << zero::types::type_name<decltype(c)>() << std::endl;
-    // for (long value : *c)
-    //     std::cout << " - [const Container<T>*] Value: " << value << std::endl;
+    // run_containers_examples();
+    // run_output_iterator_examples();
     
+    return 0;
+}
+
+
+void run_containers_examples() {
+    constexpr auto a = collections::Array<long, 5>{1L, 2L, 3L, 4L, 5L};
+    const Container<collections::Array<long, 5>>& b = collections::Array<long, 5>{1L, 2L, 3L, 4L, 5L};
+    Container<collections::Array<long, 5>>* c = new collections::Array<long, 5>{1L, 2L, 3L, 4L, 5L};
+
+    std::cout << "Iterating over the values of a constexpr zero::collection!" << std::endl;
+    std::cout << "decltype a: " << zero::types::type_name<decltype(a)>() << std::endl;
+    for (long value : a)
+        std::cout << " - [constexpr] Value: " << value << std::endl;
+
+    std::cout << "Iterating over the values of a zero::container!" << std::endl;
+    std::cout << "decltype b: " << zero::types::type_name<decltype(b)>() << std::endl;
+    for (long value : b)
+        std::cout << " - [const Container<T>&] Value: " << value << std::endl;
+
+    std::cout << "Iterating over the values of a zero::container dynamically allocated!" << std::endl;
+    std::cout << "decltype c: " << zero::types::type_name<decltype(c)>() << std::endl;
+    for (long value : *c)
+        std::cout << " - [const Container<T>*] Value: " << value << std::endl;
+}
+
+void run_output_iterator_examples() {
     std::cout << "Using output iterator with ostream: ";
     zero::iterator::legacy::output_iter<std::ostream> out1(std::cout);
     out1 = 1;
@@ -81,5 +95,4 @@ int main() {
         std::cout << x << ' ';
 
     std::cout << '\n';
-    return 0;
 }
