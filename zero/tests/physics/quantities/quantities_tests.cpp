70d0b7da82bc0af0b8d673ddae21432cc8a6b8c3
Alex Vergara <pyzyrya@tutanota.com>
2023-04-01 21:19:51 +0200

    physics-library - Unit tests for the derived magnitudes

--- zero/tests/physics/quantities/quantities_tests.cpp
+++ zero/tests/physics/quantities/quantities_tests.cpp
@@ -11,33 +11,45 @@ using namespace zero::physics;
 
 auto q1 = quantity<Kilogram, double>{3000.};
 auto q2 = quantity<Hectogram, int>{72};
-auto m1 = quantity<Meter>{5};
-auto m2 = quantity<Meter>{10};
+
+constexpr auto velocity = quantity<MetersPerSecond> {300'000.};
+constexpr auto velocity2 = quantity<KilometersPerHour> {200.};
 
 SCENARIO("Scenario: when we define some physical quantities in terms of magnitudes", "[quantity<Magnitude, T>...]") {
 
     WHEN("we perform an addition arithmetic operation in the form of the binary expression of x + y") {
-        auto addition = q1 + q2;
-        THEN("We are able to add magnitudes with the same dimension") {
-            REQUIRE(addition.amount == 3007.2);
+        auto bases_addition = q1 + q2;
+        auto derived_addition = velocity + velocity2;
+        THEN("We are able to add quantities with the same dimension") {
+            REQUIRE(bases_addition.amount == 3007.2);
+            REQUIRE_THAT(derived_addition.amount, Catch::WithinAbs(300055.555555556, 0.01));
         }
     }
+
     WHEN("we perform a subtraction arithmetic operation in the form of the binary expression of x - y") {
-        auto subtraction = q1 - q2;
-        THEN("We are able to add magnitudes with the same dimension") {
-            REQUIRE(subtraction.amount == 2992.8);
+        auto bases_subtraction = q1 - q2;
+        auto derived_subtraction = velocity - velocity2;
+        THEN("We are able to subtract quantities with the same dimension") {
+            REQUIRE(bases_subtraction.amount == 2992.8);
+            REQUIRE_THAT(derived_subtraction.amount, Catch::WithinAbs(299944.444444444, 0.01));
         }
     }
+
     WHEN("we perform a multiplication in the form of the binary expression of x * y") {
-        auto multiplication = q1 * q2;
-        THEN("We are able to multiply magnitudes with the same dimension") {
-            REQUIRE(multiplication.amount == 21600000.0);
+        auto bases_multiplication = q1 * q2;
+        auto derived_multiplication = velocity * velocity2;
+        THEN("We are able to multiply quantities with the same dimension") {
+            REQUIRE(bases_multiplication.amount == 21600000.0);
+            REQUIRE_THAT(derived_multiplication.amount, Catch::WithinAbs(16666666.6666667, 0.01));
         }
     }
+
     WHEN("we perform a division in the form of the binary expression of x / y") {
-        auto division = q1 / q2;
-        THEN("We are able to divide magnitudes with the same dimension") {
-            REQUIRE_THAT(division.amount, Catch::WithinAbs(0.42, 0.01));
+        auto bases_division = q1 / q2;
+        auto derived_division = velocity / velocity2;
+        THEN("We are able to divide quantities with the same dimension") {
+            REQUIRE_THAT(bases_division.amount, Catch::WithinAbs(0.42, 0.01));
+            REQUIRE(derived_division.amount == 5400);
         }
     }
 }
