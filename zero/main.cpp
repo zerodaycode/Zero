28774bdc4149fa0473deab75c5b040be90b9c64b
Alex Vergara <pyzyryab@tutanota.com>
2023-07-16 14:48:26 +0200

    Zero - test_suite 1. Assertions have now their own module 2. Deleted the default constructor for the test suites. A identifier must be generated always by the user

--- zero/main.cpp
+++ zero/main.cpp
@@ -7,19 +7,26 @@ void testAddition() {
     assertEquals(4, result);
 }
 
+// Passing two pointers to compare if the values that they point to are equals
+void testPtrsAddition() {
+    int result = 2 + 2;
+    int expected = 4;
+    assertEquals(&expected, &result);
+}
+
 // Driver code
 int main() {
     // Register test cases using function pointers
-    TEST_CASE("Addition Test", testAddition);
+    TEST_CASE("Addition Test", testPtrsAddition);
 
     // Register test cases using lambdas
     TEST_CASE("Subtraction Test", []() {
         int result = 5 - 3;
-        assertEquals(122, result);
+        assertEquals(122435, result);
     });
 
     // We can group related test into a suite
-    TestSuite suite;
+    TestSuite suite {"My Suite"};
     // Register test cases using function pointers into a test suite
     TEST_CASE(suite, "Addition Test", testAddition);
     // Forces a warning that alerts the user that the test will be discarded, since already
